43d1923e6c57b5731cc84f6da79a456f3fd0231e
Anton Rukhlov <ya.mr-adam@yandex.com>
2026-09-13 22:00:45 +0500

    Solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,39 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            hideButton.setOnClickListener {
+                titleTextView.isVisible = false
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putBoolean(TITLE_TV_VISIBILITY, binding.titleTextView.isVisible)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        binding.titleTextView.isVisible = savedInstanceState.getBoolean(TITLE_TV_VISIBILITY)
+    }
+
+    companion object {
+        private const val TITLE_TV_VISIBILITY = "titleTextView"
     }
 }
