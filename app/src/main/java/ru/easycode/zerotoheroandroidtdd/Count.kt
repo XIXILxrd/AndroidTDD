aac04ddbd9bc177351267f4f026b3ffdf3c19945
Anton Rukhlov <ya.mr-adam@yandex.com>
2026-09-13 23:24:34 +0500

    Solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,24 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.health.connect.datatypes.StepsRecord
+import java.io.Serializable
+
+interface Count {
+
+    fun increment(number: String): String
+
+    class Base(
+        private val step: Int
+    ) : Count {
+
+        init {
+            if (step < 1) {
+                throw IllegalStateException("step should be positive, but was $step")
+            }
+        }
+
+        override fun increment(number: String): String {
+            return (step + number.toInt()).toString()
+        }
+    }
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,42 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var _count: Count = Count.Base(step = 2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            incrementButton.setOnClickListener {
+                countTextView.text = _count.increment(number = countTextView.text.toString())
+            }
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(COUNT_KEY, binding.countTextView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        binding.countTextView.text = savedInstanceState.getString(COUNT_KEY)
+    }
+
+    companion object {
+        private const val COUNT_KEY = "count_key"
+    }
+
 }
